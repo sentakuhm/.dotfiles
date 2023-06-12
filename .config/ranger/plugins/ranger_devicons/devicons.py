6893ac2642b0ff0ea906e1268dadf5b5e30043d5
sentakuhm <sentakuhm@gmail.com>
2023-06-12 15:03:10 +0100

    Update

--- .config/ranger/plugins/ranger_devicons/devicons.py
+++ .config/ranger/plugins/ranger_devicons/devicons.py
@@ -3,21 +3,26 @@
 # These glyphs, and the mapping of file extensions to glyphs
 # has been copied from the vimscript code that is present in
 # https://github.com/ryanoasis/vim-devicons
-import re;
-import os;
 
-# Get the XDG_USER_DIRS directory names from enviromental variables
+import re
+import os
 
-xdgs_dirs = {path.split('/')[-2]: icon for key, icon in [
-    ('XDG_DOCUMENTS_DIR'  , ''),
-    ('XDG_DOWNLOAD_DIR'   , ''),
-    ('XDG_CONFIG_DIR'     , ''),
-    ('XDG_MUSIC_DIR'      , ''),
-    ('XDG_PICTURES_DIR'   , ''),
-    ('XDG_PUBLICSHARE_DIR', ''),
-    ('XDG_TEMPLATES_DIR'  , ''),
-    ('XDG_VIDEOS_DIR'     , ''),
-] if (path := os.getenv(key))}
+
+# Get the XDG_USER_DIRS directory names from environment variables
+xdgs_dirs = {
+    os.path.basename(os.getenv(key).rstrip('/')): icon
+    for key, icon in (
+        ('XDG_DOCUMENTS_DIR', ''),
+        ('XDG_DOWNLOAD_DIR', ''),
+        ('XDG_CONFIG_DIR', ''),
+        ('XDG_MUSIC_DIR', ''),
+        ('XDG_PICTURES_DIR', ''),
+        ('XDG_PUBLICSHARE_DIR', ''),
+        ('XDG_TEMPLATES_DIR', ''),
+        ('XDG_VIDEOS_DIR', ''),
+    )
+    if os.getenv(key)
+}
 
 
 # all those glyphs will show as weird squares if you don't have the correct patched font
@@ -65,8 +70,13 @@ file_node_extensions = {
     'deb'      : '',
     'diff'     : '',
     'dll'      : '',
+    'wps'      : '',
+    'wpt'      : '',
     'doc'      : '',
     'docx'     : '',
+    'docm'     : '',
+    'dotx'     : '',
+    'dotm'     : '',
     'dump'     : '',
     'edn'      : '',
     'eex'      : '',
@@ -147,14 +157,24 @@ file_node_extensions = {
     'nix'      : '',
     'o'        : '',
     'ogg'      : '',
+    'part'     : '',
     'pdf'      : '',
     'php'      : '',
     'pl'       : '',
     'pm'       : '',
     'png'      : '',
     'pp'       : '',
+    'dps'      : '',
+    'dpt'      : '',
     'ppt'      : '',
     'pptx'     : '',
+    'pptm'     : '',
+    'pot'      : '',
+    'potx'     : '',
+    'potm'     : '',
+    'pps'      : '',
+    'ppsx'     : '',
+    'ppsm'     : '',
     'ps1'      : '',
     'psb'      : '',
     'psd'      : '',
@@ -193,6 +213,7 @@ file_node_extensions = {
     'tex'      : 'ﭨ',
     'tgz'      : '',
     'toml'     : '',
+    'torrent'  : '',
     'ts'       : '',
     'tsx'      : '',
     'twig'     : '',
@@ -206,8 +227,17 @@ file_node_extensions = {
     'xbps'     : '',
     'xcplayground' : '',
     'xhtml'    : '',
+    'et'       : '',
+    'ett'      : '',
     'xls'      : '',
+    'xlt'      : '',
     'xlsx'     : '',
+    'xlsm'     : '',
+    'xlsb'     : '',
+    'xltx'     : '',
+    'xltm'     : '',
+    'xla'      : '',
+    'xlam'     : '',
     'xml'      : '',
     'xul'      : '',
     'xz'       : '',
@@ -216,6 +246,7 @@ file_node_extensions = {
     'zip'      : '',
     'zsh'      : '',
 }
+
 
 dir_node_exact_matches = {
 # English
@@ -230,6 +261,10 @@ dir_node_exact_matches = {
     'Public'                           : '',
     'Templates'                        : '',
     'Videos'                           : '',
+    'anaconda3'                        : '',
+    'go'                               : '',
+    'workspace'                        : '',
+    'OneDrive'                         : '',
 # Spanish
     'Escritorio'                       : '',
     'Documentos'                       : '',
@@ -279,9 +314,32 @@ dir_node_exact_matches = {
     'Letöltések'                       : '',
     'Számítógép'                       : '',
     'Videók'                           : '',
-# XDG_USER_DIRS
-    **xdgs_dirs
+# Chinese(Simple)
+    '桌面'                             : '',
+    '文档'                             : '',
+    '下载'                             : '',
+    '音乐'                             : '',
+    '图片'                             : '',
+    '公共的'                           : '',
+    '公共'                           : '',
+    '模板'                             : '',
+    '视频'                             : '',
+# Chinese(Traditional)
+    '桌面'                             : '',
+    '文檔'                             : '',
+    '下載'                             : '',
+    '音樂'                             : '',
+    '圖片'                             : '',
+    '公共的'                           : '',
+    '公共'                           : '',
+    '模板'                             : '',
+    '視頻'                             : '',
 }
+
+# Python 2.x-3.4 don't support unpacking syntex `{**dict}`
+# XDG_USER_DIRS
+dir_node_exact_matches.update(xdgs_dirs)
+
 
 file_node_exact_matches = {
     '.bash_aliases'                    : '',
@@ -374,6 +432,9 @@ file_node_exact_matches = {
     'webpack.config.js'                : '',
 }
 
+
 def devicon(file):
-  if file.is_directory: return dir_node_exact_matches.get(file.relative_path, '')
-  return file_node_exact_matches.get(os.path.basename(file.relative_path), file_node_extensions.get(file.extension, ''))
+    if file.is_directory:
+        return dir_node_exact_matches.get(file.relative_path, '')
+    return file_node_exact_matches.get(os.path.basename(file.relative_path),
+                                       file_node_extensions.get(file.extension, ''))
