5640bbdb62657a81d9720d482a745e08d74fad8b
sentakuhm <sentakuhm@gmail.com>
2023-11-08 17:53:54 +0100

    Update

---
+++ .config/hypr/scripts/sleep.sh
@@ -0,0 +1,5 @@
+swayidle -w timeout 300 'swaylock -f -c 000000' \
+            timeout 600 'hyprctl dispatch dpms off' \
+            resume 'hyprctl dispatch dpms on' \
+            timeout 900 'systemctl suspend' \
+            before-sleep 'swaylock -f -c 000000' &
