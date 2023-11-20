5261f5f176709929620aaef84f46641be1ba94c1
sentakuhm <sentakuhm@gmail.com>
2023-11-20 18:28:48 +0100

    Update

--- .config/hypr/waybar/scripts/mediaplayer.py
+++ .config/hypr/waybar/scripts/mediaplayer.py
@@ -3,85 +3,95 @@ import argparse
 import logging
 import sys
 import signal
-import gi
 import json
-gi.require_version('Playerctl', '2.0')
+import gi
 from gi.repository import Playerctl, GLib
+
+gi.require_version("Playerctl", "2.0")
 
 logger = logging.getLogger(__name__)
 
 is_button = False
 
+
 def write_output(text, player):
-    logger.info('Writing output')
+    logger.info("Writing output")
 
-    output = {'text': text,
-              'class': 'custom-' + player.props.player_name,
-              'alt': player.props.player_name}
+    output = {
+        "text": text,
+        "class": "custom-" + player.props.player_name,
+        "alt": player.props.player_name,
+    }
 
-    sys.stdout.write(json.dumps(output) + '\n')
+    sys.stdout.write(json.dumps(output) + "\n")
     sys.stdout.flush()
 
 
 def on_play(player, status, manager):
-    logger.info('Received new playback status')
+    logger.info("Received new playback status")
     on_metadata(player, player.props.metadata, manager)
 
-def on_metadata(player, metadata, manager):
-    logger.info('Received new metadata')
-    track_info = ''
 
-    if player.props.player_name == 'spotify' and \
-            'mpris:trackid' in metadata.keys() and \
-            ':ad:' in player.props.metadata['mpris:trackid']:
-        track_info = 'AD PLAYING'
-    elif player.get_artist() != '' and player.get_title() != '':
-        track_info = '{artist} - {title}'.format(artist=player.get_artist(),
-                                                 title=player.get_title())
+def on_metadata(player, metadata, manager):
+    logger.info("Received new metadata")
+    track_info = ""
+
+    if (
+        player.props.player_name == "spotify"
+        and "mpris:trackid" in metadata.keys()
+        and ":ad:" in player.props.metadata["mpris:trackid"]
+    ):
+        track_info = "AD PLAYING"
+    elif player.get_artist() != "" and player.get_title() != "":
+        track_info = "{artist} - {title}".format(
+            artist=player.get_artist(), title=player.get_title()
+        )
     else:
         track_info = player.get_title()
 
     #         
     if is_button:
-        if player.props.status == 'Playing':
-            track_info = ' '
-        elif player.props.status == 'Paused':
-            track_info = ' '
+        if player.props.status == "Playing":
+            track_info = " "
+        elif player.props.status == "Paused":
+            track_info = " "
         else:
-            track_info = ' '
-    elif track_info == '':
-        track_info = ' '
+            track_info = " "
+    elif track_info == "":
+        track_info = " "
     write_output(track_info, player)
 
 
 def on_player_appeared(manager, player, selected_player=None):
-    if player is not None and (selected_player is None or player.name == selected_player):
+    if player is not None and (
+        selected_player is None or player.name == selected_player
+    ):
         init_player(manager, player)
     else:
         logger.debug("New player appeared, but it's not the selected player, skipping")
 
 
 def on_player_vanished(manager, player):
-    logger.info('Player has vanished')
-    
-    output = {'text': ' ' if is_button else ' '}
+    logger.info("Player has vanished")
 
-    sys.stdout.write(json.dumps(output) + '\n')
+    output = {"text": " " if is_button else " "}
+
+    sys.stdout.write(json.dumps(output) + "\n")
     sys.stdout.flush()
 
 
 def init_player(manager, player):
-    logger.debug('Initialize player: {player}'.format(player=player.name))
+    logger.debug("Initialize player: {player}".format(player=player.name))
     player = Playerctl.Player.new_from_name(player)
-    player.connect('playback-status', on_play, manager)
-    player.connect('metadata', on_metadata, manager)
+    player.connect("playback-status", on_play, manager)
+    player.connect("metadata", on_metadata, manager)
     manager.manage_player(player)
     on_metadata(player, player.props.metadata, manager)
 
 
 def signal_handler(sig, frame):
-    logger.debug('Received signal to stop, exiting')
-    sys.stdout.write('\n')
+    logger.debug("Received signal to stop, exiting")
+    sys.stdout.write("\n")
     sys.stdout.flush()
     # loop.quit()
     sys.exit(0)
@@ -91,12 +101,12 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     # Increase verbosity with every occurrence of -v
-    parser.add_argument('-v', '--verbose', action='count', default=0)
+    parser.add_argument("-v", "--verbose", action="count", default=0)
 
     # Define for which player we're listening
-    parser.add_argument('--player')
+    parser.add_argument("--player")
 
-    parser.add_argument('-b', '--button', action='store_true')
+    parser.add_argument("-b", "--button", action="store_true")
 
     return parser.parse_args()
 
@@ -104,25 +114,30 @@ def parse_arguments():
 def main():
     arguments = parse_arguments()
 
-    global is_button 
+    global is_button
     is_button = arguments.button
 
     # Initialize logging
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG,
-                        format='%(name)s %(levelname)s %(message)s')
+    logging.basicConfig(
+        stream=sys.stderr,
+        level=logging.DEBUG,
+        format="%(name)s %(levelname)s %(message)s",
+    )
 
     # Logging is set by default to WARN and higher.
     # With every occurrence of -v it's lowered by one
     logger.setLevel(max((3 - arguments.verbose) * 10, 0))
 
     # Log the sent command line arguments
-    logger.debug('Arguments received {}'.format(vars(arguments)))
+    logger.debug("Arguments received {}".format(vars(arguments)))
 
     manager = Playerctl.PlayerManager()
     loop = GLib.MainLoop()
 
-    manager.connect('name-appeared', lambda *args: on_player_appeared(*args, arguments.player))
-    manager.connect('player-vanished', on_player_vanished)
+    manager.connect(
+        "name-appeared", lambda *args: on_player_appeared(*args, arguments.player)
+    )
+    manager.connect("player-vanished", on_player_vanished)
 
     on_player_vanished(manager, None)
 
@@ -132,9 +147,11 @@ def main():
 
     for player in manager.props.player_names:
         if arguments.player is not None and arguments.player != player.name:
-            logger.debug('{player} is not the filtered player, skipping it'
-                         .format(player=player.name)
-                         )
+            logger.debug(
+                "{player} is not the filtered player, skipping it".format(
+                    player=player.name
+                )
+            )
             continue
 
         init_player(manager, player)
@@ -142,5 +159,5 @@ def main():
     loop.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
