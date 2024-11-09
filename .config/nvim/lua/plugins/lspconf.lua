b0a150297ee0b5d1d13545d03314e764745db485
sentakuhm <sentakuhm@gmail.com>
2024-11-09 09:52:28 +0100

    Update

---
+++ .config/nvim/lua/plugins/lspconf.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  },
+}
